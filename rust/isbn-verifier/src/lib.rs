44ceddc3ec9eef4ebdc9286df48ea86934129a3f
ishtar <ishtar@local.ovf>
2022-08-23 00:20:18 +0300

    2022-08-23: rust

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -6,7 +6,9 @@ use std::iter::zip;
 
 pub fn is_valid_digits(isbn: &String) -> bool {
     // contains only 0-9 and X
-    isbn.chars().map(|x| if x.is_ascii_digit() || x == 'X' { 0 } else { 1 }).into_iter().sum::<i64>() == 0
+    isbn.chars()
+        .map(|x| if x.is_ascii_digit() || x == 'X' { 0 } else { 1 })
+        .sum::<i64>() == 0
 }
 
 pub fn is_correct_length(isbn: &String) -> bool {
@@ -16,31 +18,32 @@ pub fn is_correct_length(isbn: &String) -> bool {
 
 pub fn is_correct_check_char(isbn: &String) -> bool {
     // check if the mark is the last character
-    match isbn.find('X') {
-        None => true,
-        Some(ix) => ix == 9
+    match isbn.find('X') { 
+        Some(ix) => ix == 9,
+        _ => true
     }
 }
 
-pub fn compute_isbn_sum(isbn: String) -> i64 {
+pub fn compute_isbn_sum(isbn: &String) -> i64 {
     // compute the formula result
     zip(
-        isbn.chars().collect::<Vec<char>>().into_iter().map(|x| if x == 'X' { 10 } else { x.to_digit(10).unwrap() as i64}),
+        isbn.chars().map(|x| if x == 'X' { 10 } else if let Some(_x) = x.to_digit(10) { _x as i64 } else { 0i64 }),
         vec![10,9,8,7,6,5,4,3,2,1]
-    ).fold(0i64, |mut sum, (x, y)| { sum += x * y; sum } )
+    ).fold(0i64, |sum, (x, y)| sum + x * y )
 }
 
-pub fn filter_hyphens(isbn: String) -> String {
+pub fn filter_hyphens(isbn: &String) -> String {
     // remove hyphens
     isbn.chars().filter(|&x| x != '-').collect()
 }
 
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    let isbn = filter_hyphens(isbn.chars().collect());
-    match is_correct_check_char(&isbn) && is_correct_length(&isbn) && is_valid_digits(&isbn) {
-        true => compute_isbn_sum(isbn) % 11 == 0,
-        _ => false
-    }
+    let isbn = filter_hyphens(&isbn.chars().collect());
+
+    is_correct_check_char(&isbn)
+    && is_correct_length(&isbn)    
+    && is_valid_digits(&isbn)
+    && compute_isbn_sum(&isbn) % 11 == 0
 
 }
