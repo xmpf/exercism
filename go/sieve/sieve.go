52dd0364aab36bf3f07d4371ccea9d6c0a928d14
ishtar <ishtar@local.ovf>
2022-08-31 01:24:10 +0300

    2022-08-31: golang

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -3,6 +3,7 @@ package sieve
 func Sieve(limit int) []int {
 	var primes map[int]bool
 	var ret []int
+
 	for i := 2; i < limit; i += 1 {
 		if _, exists := primes[i]; !exists {
 			continue
@@ -11,12 +12,7 @@ func Sieve(limit int) []int {
 		primes[i] = true
 		ret = append(ret, i)
 
-		next := i
-		for {
-			next += i
-			if next >= limit {
-				break
-			}
+		for next := i * 2; next < limit; next += i {
 			primes[next] = false
 		}
 	}
